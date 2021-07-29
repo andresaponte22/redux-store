950643eea7b73ef78fe1867332ad50d443d70aca
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-29 18:58:45 -0400

    Reformatted reducers, and started components

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -93,6 +93,6 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+// export function useProductReducer(initialState) {
+//   return useReducer(reducer, initialState);
+// }
